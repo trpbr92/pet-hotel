13a60cb55e577391f9ece2dd32c839315ac4ced8
Trace Brandt <trpbr92@gmail.com>
2021-05-06 13:45:25 -0500

    get and delete saga

--- src/redux/sagas/_root.saga.js
+++ src/redux/sagas/_root.saga.js
@@ -1,7 +1,11 @@
 import {all} from 'redux-saga/effects';
+import  listPetsSaga from './listpets.saga';
+import deletePetSaga from './deletepet.saga';
+
 
 export default function* rootSaga() {
     yield all([
-
+        listPetsSaga(),
+        deletePetSaga(),
     ]);
 }
---
+++ src/redux/sagas/listpets.saga.js
@@ -0,0 +1,18 @@
+import {put, takeLatest} from 'redux-saga/effects';
+import axios from 'axios';
+
+function* fetchPets() {
+    try {
+        const list = yield axios.get('/');
+        console.log('get all', list.data);
+        yield put ({type: 'SET_LIST', payload: list.data});
+    } catch {
+        console.log('ERROR in get listpets');
+    }
+}
+
+function* listPetsSaga() {
+    yield takeLatest ('FETCH_LIST', fetchPets);
+}
+
+export default listPetsSaga;
