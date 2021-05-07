f050db4a6187d5a99e583c95ba93211c2d643722
Trace Brandt <trpbr92@gmail.com>
2021-05-07 09:40:40 -0500

    post saga created

--- src/redux/sagas/_root.saga.js
+++ src/redux/sagas/_root.saga.js
@@ -1,11 +1,13 @@
 import {all} from 'redux-saga/effects';
 import  listPetsSaga from './listpets.saga';
 import deletePetSaga from './deletepet.saga';
+import addPetSaga from './addpet.saga';
 
 
 export default function* rootSaga() {
     yield all([
         listPetsSaga(),
         deletePetSaga(),
+        addPetSaga(),
     ]);
 }
